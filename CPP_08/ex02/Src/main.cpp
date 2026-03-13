da4af2fe71d668ff156b905682ed8f91f866947d
andrelencart <andre.lencart@gmail.com>
2026-03-13 19:56:33 +0000

    Change in looks on main and ready to Deliver

--- CPP_08/ex02/Src/main.cpp
+++ CPP_08/ex02/Src/main.cpp
@@ -8,7 +8,7 @@ int main()
 	mstack.push(17);
 	std::cout << mstack.top() << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+	std::cout << "Size: " << mstack.size() << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
@@ -31,7 +31,7 @@ int main()
 	lst.push_back(17);
 	std::cout << lst.back() << std::endl;
 	lst.pop_back();
-	std::cout << lst.size() << std::endl;
+	std::cout << "Size: " << lst.size() << std::endl;
 	lst.push_back(3);
 	lst.push_back(5);
 	lst.push_back(737);
