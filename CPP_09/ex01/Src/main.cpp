af494e2e66d3e5747ef0b4a6434991733d549b13
andrelencart <andre.lencart@gmail.com>
2026-04-20 19:29:21 +0100

    Done

--- CPP_09/ex01/Src/main.cpp
+++ CPP_09/ex01/Src/main.cpp
@@ -2,11 +2,11 @@
 
 int main(int ac, char **av){
 	
-	RPN rpn;
 	if (ac != 2){
 		std::cerr << "Error: Needs 2 Arguments" << std::endl;
-		exit(1);
+		return (1);
 	}
+	RPN rpn;
 	std::string result;
 	result = rpn.CalculateInput(av[1]);
 	if (result.empty())
